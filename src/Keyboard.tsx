5d66b55e9466a46541a78489c8a3dd94861d7647
csabbah <csabbah@ryerson.ca>
2022-12-24 16:51:34 -0500

    Updated Keyboard.tsx - Added map function to render the buttons for each letter of the alphabet (including CSS styling/layout)

--- src/Keyboard.tsx
+++ src/Keyboard.tsx
@@ -1,3 +1,49 @@
+import styles from "./keyboard.module.css";
+const KEYS = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+
 export function Keyboard() {
-  return <div>Keyboard</div>;
+  return (
+    <div
+      style={{
+        width: "100%",
+        maxWidth: "700px",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexWrap: "wrap",
+        gap: "5px",
+      }}
+    >
+      {KEYS.map((key, index) => {
+        return <button className={`${styles.btn}`}>{key}</button>;
+      })}
+    </div>
+  );
 }
