a6f24b7e20ef7348896db167328602d344e99301
csabbah <csabbah@ryerson.ca>
2022-12-24 15:48:57 -0500

    Updated HangmanWord.tsx - The correct amount of empty underscores will render depending on chosen word & included the function to display letters if guessed letters are in the word

--- src/HangmanWord.tsx
+++ src/HangmanWord.tsx
@@ -1,3 +1,33 @@
 export function HangmanWord() {
-  return <div>Word</div>;
+  const word = "test";
+  const guessedLetters = ["e", "t"];
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        gap: "0.25em",
+        fontSize: "6rem",
+        fontWeight: "bold",
+        textTransform: "uppercase",
+        fontFamily: "monospace",
+      }}
+    >
+      {word.split("").map((letter, index) => {
+        return (
+          <span style={{ borderBottom: ".1em solid black" }} key={index}>
+            <span
+              style={{
+                visibility: guessedLetters.includes(letter)
+                  ? "visible"
+                  : "hidden",
+              }}
+            >
+              {letter}
+            </span>
+          </span>
+        );
+      })}
+    </div>
+  );
 }
